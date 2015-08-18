2a724c708608ee90f17eca7ee6e97075beff2c80
Laurence <liuxy6@gmail.com>
2015-08-18 22:23:33 +0800

    Update handlers

--- handlers.py
+++ handlers.py
@@ -23,15 +23,15 @@ def unknown(**args):
 
 
 def hello(**args):
-    return 'sendMessage', {'text':'Hello, I\'m Jarvis.', 'reply_to_message_id':True}
+    return 'sendMessage', {'text':'''Hello, I'm Jarvis.''', 'reply_to_message_id':True}
 
 
 def help(**args):
-    return 'sendMessage', {'text':'I don\'t want to help you because i\'m busy.', 'reply_to_message_id':True}
+    return 'sendMessage', {'text':'''I don't want to help you because I am busy.''', 'reply_to_message_id':True}
 
 
 def start(**args):
-    return 'sendMessage', {'text':'Hello, I\'m Jarvis.\nWhat can I do for you?'}
+    return 'sendMessage', {'text':'''Hello, I'm Jarvis.\nWhat can I do for you?'''}
 
 
 def sayhello(**args):
