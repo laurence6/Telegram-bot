310ad0082ce2627f25dd4a2ce9f08e7d9a4144d2
Laurence <liuxy6@gmail.com>
2015-08-22 20:32:43 +0800

    Update bot.py: Add sessions; Set bot as global variable

--- bot.py
+++ bot.py
@@ -1,17 +1,15 @@
 #! /usr/bin/env python3
-'''A telegram bot'''
+'''Telegram bot'''
 
 import logging
 import time
 
-logging.basicConfig(format='%(asctime)s [%(levelname)s] %(module)s.%(funcName)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
-
-import tgbotapi
+from tgbotapi import BOT
 from rules import text_rule, audio_rule
 
 
 def handle_message(message):
-    logger = logging.getLogger('bot.handle_message')
+    logger = logging.getLogger('bot.loop.handle_message')
     from_id = message['from']['id']
     chat_id = message['chat']['id']
     message_id = message['message_id']
@@ -19,59 +17,78 @@ def handle_message(message):
     f = None
     a = {}
 
-    if 'text' in message:
-        text = message['text']
-        f, a = text_rule.get(text)
-    elif 'photo' in message:
-        pass
-    elif 'audio' in message:
-        f, a = audio_rule.get('')
-    elif 'video' in message:
-        pass
-    elif 'document' in message:
-        pass
+    try:
+        if sessions[from_id][chat_id] == 'Deny':
+            return f, a
+    except KeyError:
+        sessions[from_id] = {}
+        sessions[from_id][chat_id] = 'OK'
 
-    a['chat_id'] = chat_id
-    a['reply_to_message_id'] = message_id if 'reply_to_message_id' in a else None
-    return f, a
+    try:
+        if 'text' in message:
+            text = message['text']
+            f, a = text_rule.get(text)
+        elif 'photo' in message:
+            pass
+        elif 'audio' in message:
+            f, a = audio_rule.get('')
+        elif 'video' in message:
+            pass
+        elif 'document' in message:
+            pass
+
+        if f:
+            a['chat_id'] = chat_id
+            a['reply_to_message_id'] = message_id if 'reply_to_message_id' in a else None
+            r = getattr(bot, f)(**a)
+            if not r['ok']:
+                logger.error(f, a)
+    except Exception as e:
+        logger.error(e)
 
 
-def loop(bot, offset=0, limit=100, timeout=15):
-    logger = logging.getLogger('bot')
+def loop(offset=0, limit=100, timeout=15):
+    logger = logging.getLogger('bot.loop')
     while 1:
-        response = tgbot.getUpdates(offset, limit, timeout)
-        if not response['ok']:
-            logger.warning('getUpdates: %s', response['description'])
-            continue
-        result = response['result']
-        for r in result:
-            f, a = handle_message(r['message'])
-            if f:
-                r = getattr(bot, f)(**a)
-                if not r['ok']:
-                    logger.error(f, a)
-        if result:
-            offset = result[-1]['update_id']+1
-            if len(result) < limit:
-                time.sleep(10)
-        else:
-            time.sleep(30)
+        try:
+            response = bot.getUpdates(offset, limit, timeout)
+            if not response['ok']:
+                logger.warning('getUpdates: %s', response['description'])
+                continue
+            result = response['result']
+            if result:
+                for r in result:
+                    handle_message(r['message'])
+                offset = result[-1]['update_id']+1
+                if len(result) < limit:
+                    time.sleep(10)
+            else:
+                time.sleep(30)
+        except Exception as e:
+            logger.error(e)
+            time.sleep(1)
 
 
-if __name__ == '__main__':
+def main():
     logger = logging.getLogger('bot')
     try:
+        global bot, sessions
         with open('tgtoken') as f:
             token = f.read().rstrip()
             if not token:
                 logger.critical('Telegram token is required')
                 exit()
-            tgbot = tgbotapi.BOT(token)
+        bot = BOT(token)
         sessions = {}
-        loop(tgbot)
+        loop()
     except KeyboardInterrupt:
         logger.info('Keyboard interrupt received')
         exit()
     except FileNotFoundError:
         logger.critical('Telegram token file not found')
         exit()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s [%(levelname)-5.5s] %(module)s.%(funcName)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
+    main()
